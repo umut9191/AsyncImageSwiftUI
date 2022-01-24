f46515e8407c321d8e6aac294f026f050eff45d2
umut9191 <umutsurmeli91@gmail.com>
2022-01-25 02:02:04 +0300

    iOS 15 and latter async coding tech on swift codes added

--- AsyncImageSwiftUI/ContentView.swift
+++ AsyncImageSwiftUI/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                //this code cant run on xcode 12 because of this sported on ios 15 later. I will wriew this codes for just knowing;
+                
+//                AsyncImage(url:URL(string: "image internet link")!{image in
+//                    //here contents = what we donwload;
+//                    image.resizable().frame(with:300,height:300,aligement:.center)
+//                },placeholder:{
+//                    //here is where showing someting instead of image while donwloading progress;
+//                    ProgressView()
+//                })
+                
+                List(superHeroArray) { superhero in
+                    Text(superhero.name).font(.title3)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    
+                }.navigationTitle("Superhero Book")
+            }
+       
+        }
+        
     }
 }
 
